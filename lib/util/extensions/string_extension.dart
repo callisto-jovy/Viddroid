e3f8f8959907c47e8c9a5ce732de756b9fe23260
callisto-jovy <48448458+callisto-jovy@users.noreply.github.com>
2023-04-09 20:29:48 +0200

    Errors in HSL-downloads are now handled accordingly.

--- lib/util/extensions/string_extension.dart
+++ lib/util/extensions/string_extension.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:crypto/crypto.dart';
 
@@ -20,5 +21,22 @@ extension StringExtension on String {
 
   String get toMD5 {
     return md5.convert(utf8.encode(this)).toString();
+  }
+
+  /// Taken from: https://pub.dev/documentation/eosdart/latest/eosdart/hexToUint8List.html
+  Uint8List get hexToUint8List {
+    if (length % 2 != 0) {
+      throw 'Odd number of hex digits';
+    }
+    var l = length ~/ 2;
+    var result = new Uint8List(l);
+    for (var i = 0; i < l; ++i) {
+      var x = int.parse(substring(i * 2, (2 * (i + 1))), radix: 16);
+      if (x.isNaN) {
+        throw 'Expected hex string';
+      }
+      result[i] = x;
+    }
+    return result;
   }
 }
