aedc970657b5a5e63112cf42a730eb08b8f9e2d1
callisto-jovy <48448458+callisto-jovy@users.noreply.github.com>
2023-04-14 22:39:23 +0200

    chore: Bump media_kit versions

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,13 +4,13 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   local_notifier
+  media_kit_libs_windows_video
   media_kit_video
   screen_retriever
   window_manager
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
-  media_kit_libs_windows_video
   media_kit_native_event_loop
 )
 
